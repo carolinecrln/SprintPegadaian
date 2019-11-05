fc5e2d20dfa9b19ba75cf9eb58e36e14bdb15f53
putrayusifin <52587456+putrayusifin@users.noreply.github.com>
2019-11-05 13:48:47 +0700

    Update MainClass.java

--- MainClass.java
+++ MainClass.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Version 6");
 		System.out.println("Adding Tambah");
+		System.out.println("tebak siapa yang ganteng");
 	}
 }
 
